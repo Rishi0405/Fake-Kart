82c1058c0ebcd25d3506484170c4b571e70b2498
Rishi kumar <Rishi kumar@192.168.1.3>
2017-08-06 09:24:27 +0530

    Login fixed header and dynamic translate.

--- eKart/src/main/webapp/resources/app/directive.js
+++ eKart/src/main/webapp/resources/app/directive.js
@@ -1,4 +1,4 @@
-var title = function(){
+var myTitle = function(){
 	return{
 		restrict: "E",
 		template: "<title>Easy Kart</title>",
@@ -6,5 +6,32 @@ var title = function(){
 	}
 }
 
+var fixedHeader = function($window, $timeout){
+	return{
+		restrict: "E",
+		scope: true,
+		template: '<div ng-include="templatelink"></div>',
+		controller: function($scope, $element){
+			$scope.templatelink = '';
+		},
+		link: function(scope,element,attr){
+			angular.element($window).on("scroll", function(e) {
+	            if(window.scrollY >= 200){
+	            	scope.templatelink = './resources/modules/banners/header.html';
+	            }else{
+	            	scope.templatelink = '';
+	            }
+	            $timeout(function(){scope.$digest();},5);
+	        });
+			scope.$on("$destroy", function(){
+				angular.element($window).off("scroll")
+			})
+		}
+	}
+}
+
+fixedHeader.$inject = ['$window', '$timeout'];
+
 angular.module("eKartDirective",[])
-.directive("myTitle",title)
+.directive("myTitle",myTitle)
+.directive("fixedHeader",fixedHeader)
--- eKart/src/main/webapp/resources/app/requiremodules/translateModule.js
+++ eKart/src/main/webapp/resources/app/requiremodules/translateModule.js
@@ -1,55 +1,12 @@
-var $translateProviderRef = "";
-
-var constants = function(){
-	return {
-		english: "resources/languagefiles/en.json",
-		french: "resources/languagefiles/fr.json"
-	}
-}
 var translate = function($translateProvider) {
-	$translateProviderRef = $translateProvider;
-	
-}
-var translateDefinition = function(languageService){
-	$translateProviderRef.preferredLanguage('en');
-	
-	languageService.getLanguage("english").then(function(res){
-		$translateProviderRef.translations('en', res);
-	})
-	languageService.getLanguage("french").then(function(res){
-		$translateProviderRef.translations('fr', res);
-	})
-}
-
-var getLanguage = function($http, $q){
-	return function(urls){
-		return function(language){
-			var deffer = $q.defer();
-			$http.get(urls[language]).then(function(res){
-				deffer.resolve(res.data);
-			},function(res){
-				
-			});
-			return deffer.promise
-		}
-	}
-}
-
-var languageService = function(getLanguage, urls){
-	var getLanguage = new getLanguage(urls);
-	return {
-		getLanguage: getLanguage
-	}
+	$translateProvider.useStaticFilesLoader({
+	    prefix: 'resources/languagefiles/',
+	    suffix: '.json'
+	});
+	$translateProvider.preferredLanguage('en');
 }
 
 translate.$inject = ["$translateProvider"];
-translateDefinition.$inject = ["languageService"];
-getLanguage.$inject = ["$http", "$q"];
-languageService.$inject = ["getLanguage","urls"];
 
 angular.module("translateConfig",['pascalprecht.translate'])
-.constant("urls",constants())
 .config(translate)
-.run(translateDefinition)
-.factory("getLanguage",getLanguage)
-.factory("languageService",languageService)
